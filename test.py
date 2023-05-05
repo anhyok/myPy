68e14c18501bd3ce19a3afb9009b0e75017a088a
Hyok An <anhyok@gmail.com>
2023-05-06 08:52:02 +0900

    .

--- test.py
+++ test.py
@@ -1,43 +1,106 @@
-import turtle as t
+# n = 5
+# for i in range(n):
+#     print(" "*(n-i) + "*"*(i*2-1))
+# for i in range(n, 0, -1):
+#     print(" "*(n-i) + "*"*(i*2-1))
 
-x_min = -5
-x_max = +5
+# for i in range(10, 0, -1):
+#     if i % 2 == 1:
+#         print(" "*(i//2), "*"*(10-i))
+# for i in range(2,10):
+#     if i%2 == 1:
+#         print(" "*(i//2), "*"*(10-i))
 
-y_min = -5
-y_max = +5
+# for i in range(1, 11, 2):
+#     print("{0:^11}".format("*"*i))
+# for i in range(11, 0, -2):
+#     print("{0:^11}".format("*"*i))
 
-space = 0.1
+# for i in range(5,0,-1):
+#     print(" "*i, "*"*(2*(5-i+1)-1))
+# for i in range(2, 5+1):
+#     print(" "*i, "*"*(2*(5-i+1)-1))
 
-func_list = ["y = x*x",
-             "y = abs(x)",
-             "y = 0.5*x + 1"]
+# for i in range(1, 11, 2):
+#     print("{0:^10}".format("*"*i))
+# for i in range(9, 0, -2):
+#     print("{0:^10}".format("*"*i))
 
-t.setworldcoordinates(
-    x_min, y_min, x_max, y_max)
-t.speed(0)
-t.pensize(2)
+# for x in range(1,6):
+#     print(" "*(5-x), "*"*x, end="")
+#     print("*"*(x-1))
+# for x in range(5,0,-1):
+#     print(" "*(5-x), "*"*x, end="")
+#     print("*"*(x-1))
 
+# for i in range(1,10,2):
+#     print((" "*((9-i)//2)) + ("*"*i) + (" "*((9-i)//2)))
+# for i in range(7,0,-2): 
+#     print((" "*((9-i)//2)) + ("*"*i) + (" "*((9-i)//2)))
 
-t.up()
-t.goto(x_min, 0)
-t.down()
-t.goto(x_max, 0)
+# for i in range(5):
+#     print(("*"*(i+1)).rjust(5))
 
-t.up()
-t.goto(0, y_min)
-t.down()
-t.goto(0, y_max)
+# for i in range(1,10):
+#     if i <= 5:
+#         for j in range(5-i):
+#             print(" ", end="")
+#         for j in range(2*i-1):
+#             print("*", end="")
+#         print()
+#     else:
+#         for j in range(i-5):
+#             print(" ", end="")
+#         for j in range((10-i)*2-1):
+#             print("*", end="")
+#         print()
 
-t.color("green")
-for func in func_list:
-    x = x_min
-    exec(func) # func = "y = x*x"
-    t.up()
-    t.goto(x, y)
-    t.down()
-    while x <= x_max:
-        x = x + space
-        exec(func)
-        t.goto(x, y)
-        
-t.exitonclick()
+# for i in range(0,5):
+#     i = i+1
+#     print('*' * (2*i-1))
+# for i in range(6,10):
+#     i = i-1
+#     print('*' * (17-2*i))
+
+# for x in range(9):
+#     print(" "*(9-x) + "*"*x + "*"*(x-1) + " "*(9-x))
+# for y in range(9):
+#     print(" "*y + "*"*(9-y) + "*"*(8-y))# + " "*y)
+
+# for x in range(5):
+#     print(" "*(5-x) + "*"*(x*2-1))
+# for y in range(5):
+#     print(" "*y + "*"*(9-(2*y)))
+
+# for x in range(1, 10):
+#     if x%2 == 1:
+#         y = 9-x
+#         k=y//2
+#         print(' '*k, '*'*x)
+# for x in range(7,0,-1):
+#     if x%2 == 1:
+#         y = 9-x
+#         k=y//2
+#         print(' '*k, '*'*x)
+
+# for i in range(0,5):
+#     # print(" "*(5-i) + "*"*(i*2)+1)
+#     print(" "*(5-i) + "*"*((i*2)+1))
+# for j in range(4,0,-1):
+#     # print(" "*(6-j) + "*"*(j*2)-1)
+#     print(" "*(6-j) + "*"*((j*2)-1))
+
+# for i in range(1, 10):
+#     if i <= 5:
+#         space = " " * (5-i)
+#         star = "*" * (2*i-1)
+#     else:
+#         space = " " * (i -5)
+#         star = "*" * (2*(10-i)-1)
+#     result = f"{space}{star}"
+#     print(result)
+
+for b in range(5):
+    print(" "*(5-b) + "*"*b + "*"*(b-1))
+for c in range(5):
+    print(" "*c + "*"*(5-c) + "*"*(4-c))
